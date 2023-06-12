c46e7cc890d7d326956960c802f92806fc55f061
Ivanna Fryshchyn <ivanna.fryshchyn@gmail.com>
2023-06-12 10:42:31 +0300

    allargs

--- src/main/java/com/example/interview/model/Task.java
+++ src/main/java/com/example/interview/model/Task.java
@@ -1,12 +1,14 @@
 package com.example.interview.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class Task {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
